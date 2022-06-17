90323008505633d39f0c74936f68a0e8feac82cf
D.Krastanov <dimitar.krastanov@mkodo.com>
2022-06-17 21:06:59 +0300

    initial commit

---
+++ T04. List Operations/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace T04._List_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string commnad = Console.ReadLine();
+
+            while (commnad != "End")
+            {
+                string[] tokens = commnad.Split();
+
+                if (tokens[0] == "Add")
+                {
+                    int number = int.Parse(tokens[1]);
+                    numbers.Add(number);
+                }
+                else if (tokens[0] == "Insert")
+                {
+                    int number = int.Parse(tokens[1]);
+                    int index = int.Parse(tokens[2]);
+                    if (index > numbers.Count - 1 || index < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+                    numbers.Insert(index, number);
+                }
+                else if (tokens[0] == "Remove")
+                {
+                    int index = int.Parse(tokens[1]);
+                    if (index > numbers.Count - 1 || index < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+                    numbers.RemoveAt(index);
+                }
+                else if (tokens[0] == "Shift")
+                {
+                    int count = int.Parse(tokens[2]);
+                    if (tokens[1] == "left")
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            numbers.Add(numbers[0]);
+                            numbers.RemoveAt(0);
+                        }
+                    }
+                    else if (tokens[1] == "right")
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            numbers.Insert(0, numbers[numbers.Count  -1]);
+                            numbers.RemoveAt(numbers.Count - 1);
+                        }
+                    }
+                }
+
+                commnad = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
